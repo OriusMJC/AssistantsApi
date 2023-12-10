1772520c320bd67d9951a16cda7b426970400908
OriusMJC <matiasjesuscontreras12@hotmail.com>
2023-12-10 16:24:07 -0300

    Endpoints controllers assitant updated

--- src/assistant/controllers/assistant.controller.ts
+++ src/assistant/controllers/assistant.controller.ts
@@ -24,27 +24,38 @@ export class AssistantController {
     return await this.service.createAssistant(body);
   }
 
-  @Post('create-thread')
-  @HttpCode(HttpStatus.OK)
-  async createThread(@Body() body: any) {
-    return await this.service.createThread(body);
-  }
+  // @Post('create-thread')
+  // @HttpCode(HttpStatus.OK)
+  // async createThread(@Body() body: any) {
+  //   return await this.service.createThread(body);
+  // }
 
-  @Post('create-message')
-  @HttpCode(HttpStatus.OK)
-  async createMessage(@Body() body: any) {
-    return await this.service.createMessage(body);
-  }
+  // @Post('create-message')
+  // @HttpCode(HttpStatus.OK)
+  // async createMessage(@Body() body: any) {
+  //   return await this.service.createMessage(body);
+  // }
 
-  @Get('list-messages')
-  @HttpCode(HttpStatus.OK)
-  async listMessages(@Query('thread_id') paramValue: string) {
-    return await this.service.listMessages(paramValue);
-  }
+  // @Get('list-messages')
+  // @HttpCode(HttpStatus.OK)
+  // async listMessages(@Query('thread_id') paramValue: string) {
+  //   return await this.service.listMessages(paramValue);
+  // }
 
-  @Post('run-thread')
+  // @Post('run-thread')
+  // @HttpCode(HttpStatus.OK)
+  // async runThread(@Body() body: any) {
+  //   return await this.service.runThread(body);
+  // }
+
+  @Post('interaction')
   @HttpCode(HttpStatus.OK)
-  async runThread(@Body() body: any) {
-    return await this.service.runThread(body);
+  async interactWithAssistant(@Body() body: any) {
+    try {
+      const response = await this.service.interactWithAssistant(body);
+      return { success: true, data: response };
+    } catch (error) {
+      return { success: false, error: 'Error interacting with the assistant.' };
+    }
   }
 }
